2f9f06d48f15dc2f1d3125a347e2d1a8d38cdeb0
Daniel Sarin <btasa013@edu.xamk.fi>
2025-10-19 18:32:58 +0300

    Fix env variable name

--- src/scripts/path.ts
+++ src/scripts/path.ts
@@ -1,8 +1,13 @@
 import { usePathname } from 'next/navigation';
 
+/**
+ *
+ * @param path Path that is appended at the end.
+ * @returns The base path appended with the input path.
+ */
 export function usePath(path?: string): string {
 
-  const isProd = process.env.PRODUCTION === 'production';
+  const isProd = process.env.NODE_ENV === 'production';
   const basePath = isProd ? "/new-portfolio/" : "/";
   
   return `${usePathname() === '/' ? basePath : ''}${path ?? ''}`;
