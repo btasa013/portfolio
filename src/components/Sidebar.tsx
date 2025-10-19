16ba3cca0699e810389eb5eb8a76e1ad4039b34c
Daniel Sarin <btasa013@edu.xamk.fi>
2025-10-19 18:29:24 +0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import RoundButton from "@/components/RoundButton";
 import Nav, { NavItem } from "@/components/Nav";
-import { getPath } from '@/scripts/path';
+import { usePath } from '@/scripts/path';
 
 interface SidebarProps {
   nav: NavItem[];
@@ -12,7 +12,7 @@ export default function Sidebar({ nav }: SidebarProps) {
       <div className="flex flex-col h-screen justify-between">
         <div className="h-full flex flex-col divide-y divide-neutral-800">
             <div className="flex items-center justify-center p-4">
-                <img alt="" width="128" height="128" src={getPath("icons/user.svg")} className="p-4 aspect-square rounded-full" />
+                <img alt="" width="128" height="128" src={usePath("icons/user.svg")} className="p-4 aspect-square rounded-full" />
                 <div className="pr-4">
                     <h1 className="text-2xl leading-none font-light">Daniel</h1>
                     <h2 className="text-xl leading-none font-extralight">Sarin</h2>
@@ -21,14 +21,14 @@ export default function Sidebar({ nav }: SidebarProps) {
             <div className="flex justify-between items-center p-2">
               <div className="flex gap-2">
                 <RoundButton link="https://www.linkedin.com/in/daniel-sarin-7aa7132b6/">
-                  <img alt="" width="28" height="28" src={getPath("icons/linkedin.svg")} className="aspect-square opacity-75 rounded-md" />
+                  <img alt="" width="28" height="28" src={usePath("icons/linkedin.svg")} className="aspect-square opacity-75 rounded-md" />
                 </RoundButton>
                 <RoundButton link="https://www.facebook.com/daniel.sarin.58/">
-                  <img alt="" width="28" height="28" src={getPath("icons/facebook.svg")} className="aspect-square opacity-75 rounded-md" />
+                  <img alt="" width="28" height="28" src={usePath("icons/facebook.svg")} className="aspect-square opacity-75 rounded-md" />
                 </RoundButton>
               </div>
               <div className="flex justify-end">
-                <RoundButton link={getPath("cv")} color="#212121af">
+                <RoundButton link={usePath("cv")} color="#212121af">
                   <p className="font-semibold p-2">CV</p>
                 </RoundButton>
               </div>
